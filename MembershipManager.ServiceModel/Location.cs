5430fbbab0903c304d5a4420551da800eca54b01
Xavier Goss <41086688+nightshaddow13@users.noreply.github.com>
2024-07-11 21:40:34 -0400

    Users/xavier/20 (#21)
    * Add zip code
    
    * Add address tables
    
    * Add Location to School

---
+++ MembershipManager.ServiceModel/Location.cs
@@ -0,0 +1,60 @@
+﻿using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace MembershipManager.ServiceModel;
+
+#region Base definition
+
+public class Location : AuditBase
+{
+    [AutoIncrement]
+    public int Id { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+
+    [ForeignKey(typeof(ZipCode))]
+    public int ZipCode { get; set; }
+}
+
+#endregion
+
+#region Interactions
+
+[AutoApply(Behavior.AuditQuery)]
+public class QueryLocation : QueryDb<Location> { }
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditCreate)]
+public class CreateLocation : ICreateDb<Location>, IReturn<IdResponse>
+{
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public int ZipCode { get; set; }
+}
+
+[ValidateHasRole(Roles.Committee)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditModify)]
+public class UpdateLocation : IPatchDb<Location>, IReturn<IdResponse>
+{
+    public string Description { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public int ZipCode { get; set; }
+}
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[AutoApply(Behavior.AuditSoftDelete)]
+public class DeleteLocation : IDeleteDb<Location>, IReturnVoid
+{
+    public int Id { get; set; }
+}
+
+#endregion
---
+++ MembershipManager.ServiceModel/ZipCode.cs
@@ -0,0 +1,65 @@
+﻿using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace MembershipManager.ServiceModel;
+
+#region Base definition
+
+public class ZipCode : AuditBase
+{
+    public int Id { get; set; }
+    public string City { get; set; } = string.Empty;
+    public State State { get; set; } = State.FL;
+
+    [Reference]
+    public List<Location> Locations { get; set; } = [];
+}
+
+public enum State
+{
+    FL
+}
+
+#endregion
+
+#region Interactions
+
+[AutoApply(Behavior.AuditQuery)]
+public class QueryZipCode : QueryDb<ZipCode> { }
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[AutoApply(Behavior.AuditCreate)]
+public class CreateZipCode : ICreateDb<ZipCode>, IReturn<IdResponse>
+{
+    public int Id { get; set; }
+    public string City { get; set; } = string.Empty;
+
+    [ApiAllowableValues(typeof(State))]
+    public State State { get; set; } = State.FL;
+}
+
+[ValidateHasRole(Roles.Committee)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditModify)]
+public class UpdateZipCode : IPatchDb<ZipCode>, IReturn<IdResponse>
+{
+    public string City { get; set; } = string.Empty;
+
+    [ApiAllowableValues(typeof(State))]
+    public State State { get; set; } = State.FL;
+}
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[AutoApply(Behavior.AuditSoftDelete)]
+public class DeleteZipCode : IDeleteDb<ZipCode>, IReturnVoid
+{
+    public int Id { get; set; }
+}
+
+#endregion
