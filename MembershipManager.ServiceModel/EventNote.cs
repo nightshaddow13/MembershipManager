22782ceeb318bb44330b00c441ec7c5af4f8f5ad
Xavier Goss <xgoss@live.com>
2024-07-14 21:49:03 -0400

    Add Notes

---
+++ MembershipManager.ServiceModel/EventNote.cs
@@ -0,0 +1,59 @@
+﻿using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace MembershipManager.ServiceModel;
+
+#region Base definition
+
+[UniqueConstraint(nameof(NoteId), nameof(EventId))]
+public class EventNote : AuditBase
+{
+    [AutoIncrement]
+    public int Id { get; set; }
+
+    [ForeignKey(typeof(Note))]
+    public int NoteId { get; set; }
+
+    [ForeignKey(typeof(Event))]
+    public int EventId { get; set; }
+}
+
+#endregion
+
+#region Interactions
+
+[AutoApply(Behavior.AuditQuery)]
+public class QueryEventNote : QueryDb<EventNote> { }
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditCreate)]
+public class CreateEventNote : ICreateDb<EventNote>, IReturn<IdResponse>
+{
+    public int NoteId { get; set; }
+    public int EventId { get; set; }
+}
+
+[ValidateHasRole(Roles.Committee)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditModify)]
+public class UpdateEventNote : IPatchDb<EventNote>, IReturn<IdResponse>
+{
+    public int NoteId { get; set; }
+    public int EventId { get; set; }
+}
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[AutoApply(Behavior.AuditSoftDelete)]
+public class DeleteEventNote : IDeleteDb<EventNote>, IReturnVoid
+{
+    public int Id { get; set; }
+}
+
+#endregion
---
+++ MembershipManager.ServiceModel/UnitNote.cs
@@ -0,0 +1,59 @@
+﻿using ServiceStack;
+using ServiceStack.DataAnnotations;
+
+namespace MembershipManager.ServiceModel;
+
+#region Base definition
+
+[UniqueConstraint(nameof(NoteId), nameof(UnitId))]
+public class UnitNote : AuditBase
+{
+    [AutoIncrement]
+    public int Id { get; set; }
+
+    [ForeignKey(typeof(Note))]
+    public int NoteId { get; set; }
+
+    [ForeignKey(typeof(Unit))]
+    public int UnitId { get; set; }
+}
+
+#endregion
+
+#region Interactions
+
+[AutoApply(Behavior.AuditQuery)]
+public class QueryUnitNote : QueryDb<UnitNote> { }
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditCreate)]
+public class CreateUnitNote : ICreateDb<UnitNote>, IReturn<IdResponse>
+{
+    public int NoteId { get; set; }
+    public int UnitId { get; set; }
+}
+
+[ValidateHasRole(Roles.Committee)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[ValidateHasRole(Roles.Committee)]
+[AutoApply(Behavior.AuditModify)]
+public class UpdateUnitNote : IPatchDb<UnitNote>, IReturn<IdResponse>
+{
+    public int NoteId { get; set; }
+    public int UnitId { get; set; }
+}
+
+[ValidateHasRole(Roles.Admin)]
+[ValidateHasRole(Roles.MembershipChair)]
+[ValidateHasRole(Roles.CouncilExecutive)]
+[AutoApply(Behavior.AuditSoftDelete)]
+public class DeleteUnitNote : IDeleteDb<UnitNote>, IReturnVoid
+{
+    public int Id { get; set; }
+}
+
+#endregion
