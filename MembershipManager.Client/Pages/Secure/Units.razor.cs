a829c1745c11df1650c7cedca22328f92d039e2a
Xavier Goss <xgoss@live.com>
2024-08-11 21:36:32 -0400

    Add slideouts for each type of thing including events and Schools

---
+++ MembershipManager.Client/Pages/Secure/Units.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ServiceStack;
+
+namespace MembershipManager.Client.Pages.Secure;
+
+public partial class Units
+{
+    [Inject] public JsonApiClient? Client { get; set; }
+
+    bool isNotesOpen = false;
+    bool isSchoolsOpen = false;
+    bool isEventsOpen = false;
+
+    protected void OnNotesClicked(MouseEventArgs args)
+    {
+        isNotesOpen = true;
+        StateHasChanged();
+    }
+
+    protected void OnEventsClicked(MouseEventArgs args)
+    {
+        isEventsOpen = true;
+        StateHasChanged();
+    }
+
+    protected void OnSchoolsClicked(MouseEventArgs args)
+    {
+        isSchoolsOpen = true;
+        StateHasChanged();
+    }
+}
